ab70f2940015cb63b63acf21b9cef0c66dbcf207
w1w1w1w1 <kapitonovdima979@gmail.com>
2023-05-31 02:29:05 +0300

    m change

--- lib/slot_pack/game_menu.dart
+++ lib/slot_pack/game_menu.dart
@@ -36,99 +36,97 @@ class GameMenu extends StatelessWidget {
           height: 926.h,
           fit: BoxFit.cover,
         ),
-        Center(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height / 1.4,
-            width: MediaQuery.of(context).size.width / 1.3,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Padding(
-                    padding:
-                        REdgeInsets.fromLTRB(0, AppColors.randomPadding, 0, 0),
-                    child: AutoSizeText(
-                      AppColors.appLable,
-                      wrapWords: false,
-                      maxLines: 1,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          background: Paint()
-                            ..color = AppColors.buttonColor.withOpacity(0.3),
-                          foreground: Paint()..color = AppColors.frontColor,
-                          fontSize: 50,
-                          fontWeight: FontWeight.normal,
-                          shadows: const [
-                            Shadow(
-                              offset: Offset(3, 3.0),
-                              blurRadius: 8,
-                              color: AppColors.backColor,
-                            ),
-                          ]),
-                    ),
-                  ),
-                  SizedBox(
-                    height: AppColors.randomPadding + 20.h,
-                  ),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushReplacement(
-                          MaterialPageRoute(
-                            builder: (context) => const SelectCharacter(),
+        SizedBox(
+          height: MediaQuery.of(context).size.height / 1.4,
+          width: MediaQuery.of(context).size.width / 1.3,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      REdgeInsets.fromLTRB(0, AppColors.randomPadding, 0, 0),
+                  child: AutoSizeText(
+                    AppColors.appLable,
+                    wrapWords: false,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        background: Paint()
+                          ..color = AppColors.buttonColor.withOpacity(0.3),
+                        foreground: Paint()..color = AppColors.frontColor,
+                        fontSize: 50,
+                        fontWeight: FontWeight.normal,
+                        shadows: const [
+                          Shadow(
+                            offset: Offset(3, 3.0),
+                            blurRadius: 8,
+                            color: AppColors.backColor,
                           ),
-                        );
-                      },
-                      child: GlowingButton(
-                        child: Text(
-                          'start game',
-                          style: TextStyle(
-                              color: AppColors.textButtonMenu, fontSize: 30.sp),
+                        ]),
+                  ),
+                ),
+                SizedBox(
+                  height: AppColors.randomPadding + 20.h,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => const SelectCharacter(),
                         ),
+                      );
+                    },
+                    child: GlowingButton(
+                      child: Text(
+                        'start game',
+                        style: TextStyle(
+                            color: AppColors.textButtonMenu, fontSize: 30.sp),
                       ),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => const GamePlay(startPage: 1),
-                          ),
-                        );
-                      },
-                      child: GlowingButton(
-                        child: Text(
-                          'settings',
-                          style: TextStyle(
-                              color: AppColors.textButtonMenu, fontSize: 30.sp),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const GamePlay(startPage: 1),
                         ),
+                      );
+                    },
+                    child: GlowingButton(
+                      child: Text(
+                        'settings',
+                        style: TextStyle(
+                            color: AppColors.textButtonMenu, fontSize: 30.sp),
                       ),
                     ),
                   ),
-                  Align(
-                    alignment:
-                        const Alignment(AppColors.randomPadding / 100, -0.8),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => RecordsTbl(),
-                          ),
-                        );
-                      },
-                      child: GlowingButton(
-                        child: Text(
-                          'my records',
-                          style: TextStyle(
-                              color: AppColors.textButtonMenu, fontSize: 30.sp),
+                ),
+                Align(
+                  alignment:
+                      const Alignment(AppColors.randomPadding / 100, -0.8),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => RecordsTbl(),
                         ),
+                      );
+                    },
+                    child: GlowingButton(
+                      child: Text(
+                        'my records',
+                        style: TextStyle(
+                            color: AppColors.textButtonMenu, fontSize: 30.sp),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
